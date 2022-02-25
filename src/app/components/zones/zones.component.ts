160e49331185cf3b7956aab282d706f42ab85796
Philippe Allard-Rousse <philrousse@gmail.com>
2022-02-25 13:36:39 -0500

    Initialize preparation-ex-httpclient-advanced

---
+++ src/app/components/zones/zones.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiAlarmClientService } from 'src/app/services/api-alarm-client.service'
+import { Zone } from 'src/app/models/AlarmStatus';
+import { Subscription, interval } from 'rxjs';
+
+@Component({
+  selector: 'app-zones',
+  templateUrl: './zones.component.html',
+  styleUrls: ['./zones.component.css']
+})
+export class ZonesComponent implements OnInit {
+
+  isInitialProgressBarDisplayed: boolean = true;
+  zones: Zone[] = [];
+  updateZonesSubscription!: Subscription;
+
+  constructor(private apiAlarmClientService: ApiAlarmClientService) { }
+
+  ngOnInit(): void {
+    this.updateZonesSubscription = this.apiAlarmClientService.getAlarmStatus().subscribe(alarmStatus => {
+      this.isInitialProgressBarDisplayed = false;
+      this.zones = alarmStatus.zones;
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.updateZonesSubscription != null) {
+      this.updateZonesSubscription.unsubscribe()
+    }
+  }
+
+  convZoneStatus2Icon(status: string) {
+    switch(status) {
+      case "ARMED": {
+        return "notifications_none";
+      }
+      case "TRIGGERED" : {
+        return "notifications_active";
+      }
+      default : {
+        return  "notifications_off";
+      }
+    }
+  }
+
+  convZoneStatus2Color(status: string) {
+    switch(status) {
+      case "ARMED": {
+        return "";
+      }
+      case "TRIGGERED" : {
+        return "warn";
+      }
+      default : {
+        return  "";
+      }
+    }
+  }
+
+  refresh() {
+    this.apiAlarmClientService.refresh();
+  }
+
+}
---
+++ src/app/services/api-alarm-client.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AlarmStatus } from '../models/AlarmStatus';
+import { Observable, timer, switchMap, Subject, merge } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiAlarmClientService {
+
+  readonly apiUrl: string = 'https://us-central1-portegarage.cloudfunctions.net/alarm-status-mock';
+
+  private subject: Subject<number>;
+
+  constructor(private http: HttpClient) {
+    this.subject = new Subject();
+  }
+
+  getAlarmStatus(): Observable<AlarmStatus> {
+    return merge(timer(1, 10000), this.subject.asObservable()).pipe(switchMap(() => this.http.get<AlarmStatus>(this.apiUrl)));
+  }
+
+  refresh(): void {
+    this.subject.next(0);
+  }
+}
